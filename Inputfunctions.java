36db224e76c293b0e96a3e5eb7ad630b111edc58
arpitapawar <87776881+arpitapawar@users.noreply.github.com>
2021-10-29 23:57:24 +0530

    Update Inputfunctions.java

--- Inputfunctions.java
+++ Inputfunctions.java
@@ -1,7 +1,7 @@
 public class Inputfunctions {
   public static void main(String[] args) {
    int[] a=new int[args.length];
-int b
+int b;
    for(int i = 0; i< args.length; i++) {
    a[i]=args[i];
 }
