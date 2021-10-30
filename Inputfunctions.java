54685047d84dbd94ba82a4a1acb4426b8b3725b4
arpitapawar <87776881+arpitapawar@users.noreply.github.com>
2021-10-30 09:53:49 +0530

    Update Inputfunctions.java

--- Inputfunctions.java
+++ Inputfunctions.java
@@ -2,8 +2,8 @@ public class Inputfunctions {
   public static void main(String[] args) {
    System.out.println("Number of Command Line Argument = "+args.length);
 int sum=0;
-	  int a=Integer.parseInt(args[i])
-		  int b=Integer.parseInt(args[i])
+	  int a=Integer.parseInt(args[i]);
+		  int b=Integer.parseInt(args[i]);
 		 
        sum=a+b;
 			System.out.println(String.format("Command Line Argument %d, sum));
