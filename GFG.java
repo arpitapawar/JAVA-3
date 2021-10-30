93aa1b25b1d925d45ee66dff922bf0c16ca88a84
arpitapawar <87776881+arpitapawar@users.noreply.github.com>
2021-10-30 10:01:19 +0530

    Update GFG.java

--- GFG.java
+++ GFG.java
@@ -14,7 +14,7 @@ class GFG {
     // Driven Program 
     public static void main(String args[]) 
     { 
-        int n = Integer.parseInt(args[i]); 
+        int n = Integer.parseInt(args[0]); 
         System.out.printf("Sum is %f", sum(n)); 
           
     } 
